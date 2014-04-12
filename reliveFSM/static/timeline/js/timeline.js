a89f51146aac8efe082ef42ac42dd343bea003b9
Mari Batilando <mari.batilando@gmail.com>
2014-04-12 01:12:56 -0700

    derp

--- reliveFSM/static/timeline/js/timeline.js
+++ reliveFSM/static/timeline/js/timeline.js
@@ -1,29 +1,30 @@
 $( document ).ready(function() {
     console.log( "timeline.js ready!" );
     $("#search-button").click(function() {
-    	$("#my-picture").fadeOut("slow");
-    	var searchQuery = $("#search-bar").val();
-    	console.log(searchQuery);
-    	$.ajax({
-		  type: "GET",
-		  url: "search/?q=" + searchQuery,
-		  success: function(data) {
-		    console.log(data.docs);
-		    filterDate = $("#filter-date").is(":checked");
-		    filterImage = $("#filter-image").is(":checked");
-		    if(filterDate) {
-		    	var withDate = filter("date", data.docs);
-		        data.docs = withDate;
-		    }
-		    if(filterImage) {
-		    	var withImage = filter("image", data.docs);
-		        data.docs = withImage;
-		    }
-			var template = $("#timeline-template").html();
-			var html = Mustache.render(template, data.docs);
-			$("#timeline-list").html(html);
-		  }
-		});
+    	$("#my-picture").fadeOut("slow", function() {
+    		var searchQuery = $("#search-bar").val();
+	    	console.log(searchQuery);
+	    	$.ajax({
+			  type: "GET",
+			  url: "search/?q=" + searchQuery,
+			  success: function(data) {
+			    console.log(data.docs);
+			    filterDate = $("#filter-date").is(":checked");
+			    filterImage = $("#filter-image").is(":checked");
+			    if(filterDate) {
+			    	var withDate = filter("date", data.docs);
+			        data.docs = withDate;
+			    }
+			    if(filterImage) {
+			    	var withImage = filter("image", data.docs);
+			        data.docs = withImage;
+			    }
+				var template = $("#timeline-template").html();
+				var html = Mustache.render(template, data.docs);
+				$("#timeline-list").html(html);
+			  }
+			});
+    	});
     });
 });
 
